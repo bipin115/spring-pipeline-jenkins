e3f4564fb5488fd7fe40fd8621e8a7b9261a42de
bipin115 <bipin.singh4115@gmail.com>
2021-06-19 22:25:18 +0530

    Added new spring project for Jenkins Pipeline build

---
+++ src/main/java/com/spring/build/pipeline/springpipelinedeploy/SpringPipelineDeployApplication.java
@@ -0,0 +1,13 @@
+package com.spring.build.pipeline.springpipelinedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPipelineDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPipelineDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/build/pipeline/springpipelinedeploy/SpringPipelineDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.build.pipeline.springpipelinedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPipelineDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
